8821decc3982d9ec34da4086d6727524de74691d
Sharath chandran <58880812+k00sharath@users.noreply.github.com>
2024-08-12 16:29:01 +0000

    Add memeory limit for elastic search container in docker-compose

---
+++ pull.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+echo "Starting Ollama server..."
+ollama serve &
+
+
+echo "Waiting for Ollama server to be active..."
+while [ "$(ollama list | grep 'NAME')" == "" ]; do
+  sleep 1
+done
+
+ollama pull phi3
